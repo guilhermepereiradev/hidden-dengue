2907153f917e5f2a2850e95b89ae484e6b3b93c0
WerleyL <werleyluciog@gmail.com>
2023-11-08 20:56:46 -0300

    implementação do botão de reprodução

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const botaoInstrucoes = document.getElementById("botaoInstrucoes");
 const modal = document.getElementById("modal");
 const fecharModal = document.getElementById("fecharModal");
+const audio = document.getElementById("audio");
+const botaoReproduzir = document.getElementById("botaoReproduzir");
 
 botaoInstrucoes.addEventListener("click", () => {
     modal.style.display = "block";
@@ -15,3 +17,14 @@ window.addEventListener('click', (event) => {
         modal.style.display = 'none';
     }
 });
+
+botaoReproduzir.addEventListener("click", function () {
+    if (audio.paused) {
+        audio.play();
+        botaoReproduzir.src = "img/ativado.png";
+        return;
+    }
+
+    audio.pause();
+    botaoReproduzir.src = "img/desativado.png";
+})
